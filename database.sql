524beaabdc29892d1ab028e61adb40546f50981a
WY <217013462@stu.vtc.edu.hk>
2022-09-09 15:05:42 +0800

    database sql for appointments table

--- database.sql
+++ database.sql
@@ -1,3 +1,4 @@
+-- table for users data
 CREATE TABLE IF NOT EXISTS users (
 user_id int(11) NOT NULL AUTO_INCREMENT,
 user_name_e varchar(100) NOT NULL,
@@ -12,7 +13,18 @@ user_email varchar(100) NOT NULL,
 user_salt varchar(128) NOT NULL,
 user_pwd varchar(512) NOT NULL,
 create_time datetime DEFAULT CURRENT_TIMESTAMP,
-last_update_time datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
 PRIMARY KEY (user_id),
 UNIQUE (user_id, user_hkid, user_email)
+);
+
+-- table for appointments data
+CREATE TABLE IF NOT EXISTS appointments (
+appt_id int(11) NOT NULL AUTO_INCREMENT,
+user_id int(11) NOT NULL,
+-- user_id is the foreign key linking to the users table
+appt_location varchar(30) NOT NULL,
+appt_date_time datetime NOT NULL,
+create_time datetime DEFAULT CURRENT_TIMESTAMP,
+PRIMARY KEY (appt_id),
+UNIQUE (appt_id)
 );
