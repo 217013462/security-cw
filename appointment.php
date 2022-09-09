34074548c61c0719f6b322ea4a5ff9a70598166c
WY <217013462@stu.vtc.edu.hk>
2022-09-09 13:22:20 +0800

    appointment form updated

--- appointment.php
+++ appointment.php
@@ -10,19 +10,52 @@
 ?>
 
 <h3>Make an appointment</h3>
+<p>Appointments available for the next 30 days</p>
 <form action="includes/appointment-inc.php" method="post" id="appt">
     <table width="350" border="0" cellpadding="5">
         <tr>
             <td width="100">Date</td>
             <td width="200"><label for="appt_date"></label>
-                <input type="date" name="appt_date" id="appt_date" size="40" required />
+                <input type="date" name="appt_date" id="appt_date" size="40"
+                    min="<?php echo date("Y-m-d", strtotime("+1 day")); ?>"
+                    max="<?php echo date("Y-m-d", strtotime("+30 day")); ?>" required />
             </td>
         </tr>
 
         <tr>
             <td width="100">Time</td>
             <td width="200"><label for="appt_time"></label>
-                <input type="time" name="appt_time" id="appt_time" size="40" required />
+                <select name="appt_time" id="appt_time" required>
+                    <option value="" disabled selected>Select your preference time</option>
+                    <option value="08:00">08:00</option>
+                    <option value="08:30">08:30</option>
+                    <option value="09:00">09:00</option>
+                    <option value="09:30">09:30</option>
+                    <option value="10:00">10:00</option>
+                    <option value="10:30">10:30</option>
+                    <option value="11:00">11:00</option>
+                    <option value="11:30">11:30</option>
+                    <option value="12:00">12:00</option>
+                    <option value="12:30">12:30</option>
+                    <option value="13:00">13:00</option>
+                    <option value="13:30">13:30</option>
+                    <option value="14:00">14:00</option>
+                    <option value="14:30">14:30</option>
+                    <option value="15:00">15:00</option>
+                    <option value="15:30">15:30</option>
+                    <option value="16:00">16:00</option>
+                    <option value="16:30">16:30</option>
+                    <option value="17:00">17:00</option>
+                    <option value="17:30">17:30</option>
+                    <option value="18:00">18:00</option>
+                    <option value="18:30">18:30</option>
+                    <option value="19:00">19:00</option>
+                    <option value="19:30">19:30</option>
+                    <option value="20:00">20:00</option>
+                    <option value="20:30">20:30</option>
+                    <option value="21:00">21:00</option>
+                    <option value="21:30">21:30</option>
+                </select>
             </td>
         </tr>
 
